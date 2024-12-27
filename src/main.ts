d965881e6390cc891af1e7622f278a979c5fdb9d
feredev <feredev2@gmail.com>
2024-12-27 02:44:34 -0400

    moviendo el processor a su propio archivo

--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,7 @@
-import path from 'path';
-import fs from 'fs';
 import Pais from "./models/Pais";
+import Processor from "./processor";
 import { Continentes } from "./types";
 
-const FORMATEAR_SQL = false;  // Cambia a `false` si no quieres formato
-
-class Processor {
-  // Método para ejecutar las entidades y procesar el SQL
-  run(entities: { toSQL: () => string }[]): string {
-    let sql = entities.map(entity => entity.toSQL()).join('\n');
-
-    if (!FORMATEAR_SQL) {
-      // Elimina los saltos de línea y los espacios innecesarios si FORMATEAR_SQL es false
-      sql = sql.replace(/\s+/g, ' ').trim();
-    }
-
-    return sql;
-  }
-
-  // Método estático para leer el archivo SQL y aplicar formato
-  static readSQLFile(filePath: string): string {
-    const sqlFileContent = fs.readFileSync(filePath, 'utf-8');
-
-    if (!FORMATEAR_SQL) {
-      // Elimina los saltos de línea y los espacios innecesarios si FORMATEAR_SQL es false
-      return sqlFileContent.replace(/\s+/g, ' ').trim();
-    }
-
-    return sqlFileContent;  // Devuelve sin modificar si FORMATEAR_SQL es true
-  }
-
-  // Método estático para leer el archivo SQL desde la ruta base del proyecto
-  static loadSQLFile(): string {
-    // Define la constante BASE_DIR para la ruta base de tu proyecto
-    const BASE_DIR = path.resolve(__dirname, '../'); // Esto apunta al directorio raíz del proyecto
-    // Usa la constante BASE_DIR para construir la ruta al archivo SQL
-    const sqlFilePath = path.join(BASE_DIR, 'sql', 'tables-creation.sql');
-    
-    // Leer el archivo SQL con el método estático
-    return this.readSQLFile(sqlFilePath);
-  }
-
-  // Método estático para exportar el SQL generado a un archivo .sql
-  static exportToSQLFile(fileName: string, creationSQL: string, dataSQL: string): void {
-    const outputPath = path.join(__dirname, fileName); // Ruta donde se guardará el archivo .sql
-
-    // Combina la creación de tablas con las inserciones de datos
-    const content = `${creationSQL}\n\n${dataSQL}`;
-    
-    // Escribir el contenido en el archivo (sobrescribe si ya existe)
-    fs.writeFileSync(outputPath, content, 'utf-8');
-    console.log(`Archivo SQL exportado a: ${outputPath}`);
-  }
-}
 
 // Instanciando el procesador
 const processor = new Processor();
